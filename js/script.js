4241c3fd12cba3b2409333502382c665a4ca8548
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-07-01 18:52:03 +0200

    remove task

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 
 {
-const form = document.querySelector(".js-form");
-const listElement = document.querySelector(".js-list")
+    const form = document.querySelector(".js-form");
+    const listElement = document.querySelector(".js-list")
 
-const tasks = [
-  
-];
+    const tasks = [
 
-const render = () => {
-    let htmlString = "";
-    for (const task of tasks) {
-        htmlString +=
-        `
+    ];
+    const addNewTask = (newTask) => {
+        tasks.push({
+            content: newTask,
+        });
+
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString +=
+                `
             <li 
             ${task.done ? "style =\"text-decoration: line-through\"" : ""} 
             >
@@ -21,39 +34,37 @@ const render = () => {
                
             </li>
     `
-    }
-    document.querySelector(".js-list").innerHTML = htmlString;  
-};
+        }
+        document.querySelector(".js-list").innerHTML = htmlString;
 
-    const removeButtons = document.querySelectorAll(".js-remove");
+        const removeButtons = document.querySelectorAll(".js-remove");
 
-    removeButtons.forEach((removeButton, index) => {
-    removeButton.addEventListener("click", () =>{
-        task.splice(index, 1);
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+
+
+    const init = () => {
         render();
-    });
-    });
 
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-const init = () =>{
-render();
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    const newTask = document.querySelector(".js-newTask").value.trim();
-    if (newTask === "") {
-        return;
+            const newTask = document.querySelector(".js-newTask").value.trim();
+            if (newTask === "") {
+                return;
+            }
+
+            addNewTask(newTask);
+
+        })
     }
 
-    tasks.push ({
-        content: newTask,
-    });
-
-render(); 
-
-})}
-
-init();
+    init();
 
 };
 
